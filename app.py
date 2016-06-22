821d63c180e4b86d3fc1e60787ee637b0c25249e
shannahutch <shannahutch@gmail.com>
2016-06-22 12:10:56 -0400

    Added Heroku pro and stage and launched. This is message change

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "TelemeterED!!"
+    return "TelemeterED!!  Coming soon to a GIG near you.."
 
 if __name__ == '__main__':
     app.run()
