bebc666bf41e970155cc5e160c4942b017b74d68
shannahutch <shannahutch@gmail.com>
2016-06-27 10:28:09 -0400

    fixed conifg spelling in app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
-app.config.from_onbject(os.environ['APP_SETTINGS'])
+app.config.from_object(os.environ['APP_SETTINGS'])
 # check that it is detecting the correct environ
 print(os.environ['APP_SETTINGS'])
 
