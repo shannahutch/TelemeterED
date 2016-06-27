6caaa76ab8c3a5b62979fd0599b0d9b15f3bfe77
shannahutch <shannahutch@gmail.com>
2016-06-27 13:56:13 -0400

    setting up postgreSQL and model.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 import os
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 # check that it is detecting the correct environ
-print(os.environ['APP_SETTINGS'])
+# print(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+from models import Result
 
 
 @app.route('/')
